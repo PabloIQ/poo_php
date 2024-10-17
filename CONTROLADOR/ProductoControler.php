0998ad2ea3170b63368a4c7eb336f2934faa088c
Pablo Ignacio <ignacio.quintero.pablo@iescamas.es>
2024-10-17 09:17:54 +0200

    Añadido clase ProductoControler

---
+++ CONTROLADOR/ProductoControler.php
@@ -0,0 +1,17 @@
+<?php
+class ProductoControler {
+    //private $Servicio;
+
+    public function __construct() {
+        //Aqui creamos la construccion del servicio
+    }
+
+    public function listarProducto() {
+        //Usar el atributo servicio y traerse los datos de la BBDD para mostrarlo
+    }
+
+    public function ejecuta() {
+        $productos = $this-> listarProducto();
+    }
+}
+?>
